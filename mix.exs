a971d32b651fcc67e6dd4a6b2cf0e4d5d90373d7
Jonathan Cubides <jonathan.cubides@uib.no>
2025-05-20 13:27:07 -0500

    fix: project setup and lint config

--- mix.exs
+++ mix.exs
@@ -1,29 +1,46 @@
-defmodule Engine.MixProject do
+defmodule EngineSystem.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :engine,
+      app: :engine_system,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.14",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: ["lib", "examples"]
+      description: "A formal model-adherent implementation of distributed engines",
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  # Add this function to define compilable paths
+  # defp elixirc_paths(:test), do: ["lib", "test/support", "examples"]
+  defp elixirc_paths(_), do: ["lib", "examples"]
+
+  # Run "mix help compile.app" to learn about applications
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {EngineSystem.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  # Run "mix help deps" to learn about dependencies
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:typed_struct, "~> 0.3.0"},
+      {:uuid, "~> 1.1"},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/anoma/jonaprieto"}
     ]
   end
 end
