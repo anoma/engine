000c212e70276fdfeaf9c960492f145b56d2dfcc
Jonathan Cubides <jonathan.cubides@uib.no>
2025-05-25 01:02:56 -0500

    refactor!: remove old type system
    BREAKING CHANGE: Removed legacy type definitions including types.ex and all type modules (definitions, engine_instance_info, engine_mode, engine_status, engine_type_info, message_envelope, operation_result, system_info). Types are now embedded in their respective modules.

--- lib/engine_system/types/definitions.ex
+++
@@ -1,9 +0,0 @@
-defmodule EngineSystem.Types.Definitions do
-  @moduledoc """
-  I define common types used throughout the EngineSystem.
-  """
-
-  @type engine_address :: any()
-  @type message_id :: String.t()
-  @type timestamp :: System.time_unit()
-end
