6517722da9e0667860f46ca6c770fc470bd22a1a
Jonathan Cubides <jonathan.cubides@uib.no>
2025-06-02 02:26:37 -0500

    build: add Dialyzer ignore rules for future-proofing code

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,4 +2,9 @@
   # Add any dialyzer warnings to ignore here if needed
   # Example:
   # ~r"lib/some_module.ex:123:pattern_match"
+
+  # Ignore warnings for mailbox update function - this code is future-proofing
+  # for when mailbox engines (mode: :mailbox) are actually used in the system
+  ~r"lib/engine_system/system/spawner.ex:362:contract_supertype",
+  ~r"lib/engine_system/system/spawner.ex:363:.*:pattern_match"
 ]
