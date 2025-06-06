d7e277c939238bfccb594ff16735f55b9d0817ef
Jonathan Cubides <jonathan.cubides@uib.no>
2025-06-06 02:33:04 +0200

    chore: fix formatting

--- lib/examples/dsl_mailbox_simple.ex
+++ lib/examples/dsl_mailbox_simple.ex
@@ -21,21 +21,22 @@ defmodule Examples.DSLMailboxSimple do
   defengine KVProcessingEngine do
     @moduledoc "Example key-value processing engine using unified DSL."
 
-    version "1.0.0"
-    mode :process  # This is a processing engine
+    version("1.0.0")
+    # This is a processing engine
+    mode(:process)
 
     # Processing engine interface - what messages it can handle
     interface do
-      message :put, key: :atom, value: :any
-      message :get, key: :atom
-      message :delete, key: :atom
-      message :clear_all
-      message :list_keys
-      message :get_stats
+      message(:put, key: :atom, value: :any)
+      message(:get, key: :atom)
+      message(:delete, key: :atom)
+      message(:clear_all)
+      message(:list_keys)
+      message(:get_stats)
       # Response messages
-      message :result, value: :any
-      message :ack
-      message :error, reason: :atom
+      message(:result, value: :any)
+      message(:ack)
+      message(:error, reason: :atom)
     end
 
     config do
@@ -56,7 +57,7 @@ defmodule Examples.DSLMailboxSimple do
       }
     end
 
-    message_filter fn _msg, _config, _env -> true end
+    message_filter(fn _msg, _config, _env -> true end)
 
     # Processing engine behavior - the business logic
     behaviour do
@@ -65,84 +66,93 @@ defmodule Examples.DSLMailboxSimple do
           {:ok, [{:send, sender, {:error, :max_keys_reached}}]}
         else
           new_store = Map.put(env.store, key, value)
-          new_access_counts = if config.enable_stats do
-            Map.update(env.access_counts, key, 1, &(&1 + 1))
-          else
-            env.access_counts
-          end
 
-          new_env = %{env |
-            store: new_store,
-            access_counts: new_access_counts,
-            total_operations: env.total_operations + 1
+          new_access_counts =
+            if config.enable_stats do
+              Map.update(env.access_counts, key, 1, &(&1 + 1))
+            else
+              env.access_counts
+            end
+
+          new_env = %{
+            env
+            | store: new_store,
+              access_counts: new_access_counts,
+              total_operations: env.total_operations + 1
           }
 
-          {:ok, [
-            {:update_environment, new_env},
-            {:send, sender, :ack}
-          ]}
+          {:ok,
+           [
+             {:update_environment, new_env},
+             {:send, sender, :ack}
+           ]}
         end
       end
 
       on_message :get, %{key: key}, config, env, sender do
         value = Map.get(env.store, key)
 
-        new_env = if config.enable_stats do
-          new_access_counts = Map.update(env.access_counts, key, 1, &(&1 + 1))
-          %{env |
-            access_counts: new_access_counts,
-            total_operations: env.total_operations + 1
-          }
-        else
-          %{env | total_operations: env.total_operations + 1}
-        end
+        new_env =
+          if config.enable_stats do
+            new_access_counts = Map.update(env.access_counts, key, 1, &(&1 + 1))
+            %{env | access_counts: new_access_counts, total_operations: env.total_operations + 1}
+          else
+            %{env | total_operations: env.total_operations + 1}
+          end
 
-        {:ok, [
-          {:update_environment, new_env},
-          {:send, sender, {:result, value}}
-        ]}
+        {:ok,
+         [
+           {:update_environment, new_env},
+           {:send, sender, {:result, value}}
+         ]}
       end
 
       on_message :delete, %{key: key}, _config, env, sender do
         if Map.has_key?(env.store, key) do
           new_store = Map.delete(env.store, key)
           new_access_counts = Map.delete(env.access_counts, key)
-          new_env = %{env |
-            store: new_store,
-            access_counts: new_access_counts,
-            total_operations: env.total_operations + 1
+
+          new_env = %{
+            env
+            | store: new_store,
+              access_counts: new_access_counts,
+              total_operations: env.total_operations + 1
           }
 
-          {:ok, [
-            {:update_environment, new_env},
-            {:send, sender, :ack}
-          ]}
+          {:ok,
+           [
+             {:update_environment, new_env},
+             {:send, sender, :ack}
+           ]}
         else
           {:ok, [{:send, sender, {:error, :key_not_found}}]}
         end
       end
 
       on_message :clear_all, %{}, _config, env, sender do
-        new_env = %{env |
-          store: %{},
-          access_counts: %{},
-          total_operations: env.total_operations + 1
+        new_env = %{
+          env
+          | store: %{},
+            access_counts: %{},
+            total_operations: env.total_operations + 1
         }
 
-        {:ok, [
-          {:update_environment, new_env},
-          {:send, sender, :ack}
-        ]}
+        {:ok,
+         [
+           {:update_environment, new_env},
+           {:send, sender, :ack}
+         ]}
       end
 
       on_message :list_keys, %{}, _config, env, sender do
         keys = Map.keys(env.store)
         new_env = %{env | total_operations: env.total_operations + 1}
 
-        {:ok, [
-          {:update_environment, new_env},
-          {:send, sender, {:result, keys}}
-        ]}
+        {:ok,
+         [
+           {:update_environment, new_env},
+           {:send, sender, {:result, keys}}
+         ]}
       end
 
       on_message :get_stats, %{}, _config, env, sender do
@@ -165,14 +175,15 @@ defmodule Examples.DSLMailboxSimple do
   defengine SimpleFIFOMailbox, compile: true do
     @moduledoc "Simple FIFO mailbox engine using unified DSL."
 
-    version "1.0.0"
-    mode :mailbox                    # This is a mailbox engine
+    version("1.0.0")
+    # This is a mailbox engine
+    mode(:mailbox)
 
     # Standard mailbox interface
     interface do
-      message :enqueue_message, message: :any
-      message :request_batch, demand: :integer
-      message :update_filter, filter: :function
+      message(:enqueue_message, message: :any)
+      message(:request_batch, demand: :integer)
+      message(:update_filter, filter: :function)
     end
 
     config do
@@ -194,16 +205,13 @@ defmodule Examples.DSLMailboxSimple do
       }
     end
 
-    message_filter fn _msg, _config, _env -> true end
+    message_filter(fn _msg, _config, _env -> true end)
 
     # Implement simple FIFO delivery policy in behaviour
     behaviour do
       on_message :enqueue_message, %{message: message}, _config, env, _sender do
         new_queue = :queue.in(message, env.message_queue)
-        new_env = %{env |
-          message_queue: new_queue,
-          total_received: env.total_received + 1
-        }
+        new_env = %{env | message_queue: new_queue, total_received: env.total_received + 1}
         {:ok, [{:update_environment, new_env}]}
       end
 
@@ -211,16 +219,18 @@ defmodule Examples.DSLMailboxSimple do
         batch_size = min(demand, config.batch_size)
         {messages, new_queue} = extract_messages(env.message_queue, batch_size, [])
 
-        new_env = %{env |
-          message_queue: new_queue,
-          current_demand: env.current_demand + demand,
-          total_delivered: env.total_delivered + length(messages)
+        new_env = %{
+          env
+          | message_queue: new_queue,
+            current_demand: env.current_demand + demand,
+            total_delivered: env.total_delivered + length(messages)
         }
 
-        {:ok, [
-          {:update_environment, new_env},
-          {:send, sender, {:deliver_batch, messages}}
-        ]}
+        {:ok,
+         [
+           {:update_environment, new_env},
+           {:send, sender, {:deliver_batch, messages}}
+         ]}
       end
 
       on_message :update_filter, %{filter: _new_filter}, _config, _env, sender do
@@ -234,7 +244,9 @@ defmodule Examples.DSLMailboxSimple do
 
     defp extract_messages(queue, remaining, acc) do
       case :queue.out(queue) do
-        {:empty, queue} -> {Enum.reverse(acc), queue}
+        {:empty, queue} ->
+          {Enum.reverse(acc), queue}
+
         {{:value, message}, new_queue} ->
           extract_messages(new_queue, remaining - 1, [message | acc])
       end
@@ -248,13 +260,13 @@ defmodule Examples.DSLMailboxSimple do
   defengine PriorityMailbox do
     @moduledoc "Priority-based mailbox engine using unified DSL."
 
-    version "1.0.0"
-    mode :mailbox
+    version("1.0.0")
+    mode(:mailbox)
 
     interface do
-      message :enqueue_message, message: :any
-      message :request_batch, demand: :integer
-      message :flush_coalesced_writes
+      message(:enqueue_message, message: :any)
+      message(:request_batch, demand: :integer)
+      message(:flush_coalesced_writes)
     end
 
     config do
@@ -277,7 +289,7 @@ defmodule Examples.DSLMailboxSimple do
       }
     end
 
-    message_filter fn _msg, _config, _env -> true end
+    message_filter(fn _msg, _config, _env -> true end)
 
     # Implement priority delivery + write coalescing in behaviour
     behaviour do
@@ -290,10 +302,7 @@ defmodule Examples.DSLMailboxSimple do
             key = extract_key(message)
             new_coalesce_buffer = Map.put(env.coalesce_buffer, key, message)
             new_stats = update_in(env.stats.received, &(&1 + 1))
-            new_env = %{env |
-              coalesce_buffer: new_coalesce_buffer,
-              stats: new_stats
-            }
+            new_env = %{env | coalesce_buffer: new_coalesce_buffer, stats: new_stats}
             {:ok, [{:update_environment, new_env}]}
 
           _ ->
@@ -302,10 +311,7 @@ defmodule Examples.DSLMailboxSimple do
             priority_key = {priority, timestamp, env.stats.received}
             new_buffer = :gb_trees.enter(priority_key, message, env.priority_buffer)
             new_stats = update_in(env.stats.received, &(&1 + 1))
-            new_env = %{env |
-              priority_buffer: new_buffer,
-              stats: new_stats
-            }
+            new_env = %{env | priority_buffer: new_buffer, stats: new_stats}
             {:ok, [{:update_environment, new_env}]}
         end
       end
@@ -316,34 +322,35 @@ defmodule Examples.DSLMailboxSimple do
         {messages, new_buffer} = extract_priority_batch(env.priority_buffer, batch_size)
 
         new_stats = update_in(env.stats.delivered, &(&1 + length(messages)))
-        new_env = %{env |
-          priority_buffer: new_buffer,
-          current_demand: env.current_demand + demand,
-          stats: new_stats
+
+        new_env = %{
+          env
+          | priority_buffer: new_buffer,
+            current_demand: env.current_demand + demand,
+            stats: new_stats
         }
 
-        {:ok, [
-          {:update_environment, new_env},
-          {:send, sender, {:deliver_batch, messages}}
-        ]}
+        {:ok,
+         [
+           {:update_environment, new_env},
+           {:send, sender, {:deliver_batch, messages}}
+         ]}
       end
 
       on_message :flush_coalesced_writes, %{}, _config, env, _sender do
         # Move coalesced writes to priority buffer
         coalesce_count = map_size(env.coalesce_buffer)
 
-        new_buffer = Enum.reduce(env.coalesce_buffer, env.priority_buffer, fn {_key, message}, buffer ->
-          timestamp = :erlang.system_time(:microsecond)
-          priority_key = {2, timestamp, env.stats.received}  # Write priority = 2
-          :gb_trees.insert(priority_key, message, buffer)
-        end)
+        new_buffer =
+          Enum.reduce(env.coalesce_buffer, env.priority_buffer, fn {_key, message}, buffer ->
+            timestamp = :erlang.system_time(:microsecond)
+            # Write priority = 2
+            priority_key = {2, timestamp, env.stats.received}
+            :gb_trees.insert(priority_key, message, buffer)
+          end)
 
         new_stats = update_in(env.stats.coalesced, &(&1 + coalesce_count))
-        new_env = %{env |
-          priority_buffer: new_buffer,
-          coalesce_buffer: %{},
-          stats: new_stats
-        }
+        new_env = %{env | priority_buffer: new_buffer, coalesce_buffer: %{}, stats: new_stats}
 
         {:ok, [{:update_environment, new_env}]}
       end
@@ -397,24 +404,34 @@ defmodule Examples.DSLMailboxSimple do
     # Both processing and mailbox engines use the same DSL syntax!
 
     # Spawn a processing engine with simple FIFO mailbox
-    {:ok, addr1} = EngineSystem.spawn_engine(
-      KVProcessingEngine,              # Processing engine (DSL-defined)
-      %{access_mode: :read_write},     # Processing config
-      %{store: %{}},                   # Processing environment
-      :kv_with_fifo,                   # Name
-      SimpleFIFOMailbox,               # Mailbox engine (ALSO DSL-defined!)
-      %{batch_size: 10}                # Mailbox config
-    )
+    {:ok, addr1} =
+      EngineSystem.spawn_engine(
+        # Processing engine (DSL-defined)
+        KVProcessingEngine,
+        # Processing config
+        %{access_mode: :read_write},
+        # Processing environment
+        %{store: %{}},
+        # Name
+        :kv_with_fifo,
+        # Mailbox engine (ALSO DSL-defined!)
+        SimpleFIFOMailbox,
+        # Mailbox config
+        %{batch_size: 10}
+      )
 
     # Spawn the same processing engine with priority mailbox
-    {:ok, addr2} = EngineSystem.spawn_engine(
-      KVProcessingEngine,              # Same processing engine
-      %{access_mode: :read_write},
-      %{store: %{}},
-      :kv_with_priority,
-      PriorityMailbox,                 # Different mailbox engine (ALSO DSL-defined!)
-      %{batch_size: 8, coalesce_window_ms: 50}
-    )
+    {:ok, addr2} =
+      EngineSystem.spawn_engine(
+        # Same processing engine
+        KVProcessingEngine,
+        %{access_mode: :read_write},
+        %{store: %{}},
+        :kv_with_priority,
+        # Different mailbox engine (ALSO DSL-defined!)
+        PriorityMailbox,
+        %{batch_size: 8, coalesce_window_ms: 50}
+      )
 
     %{fifo: addr1, priority: addr2}
   end
@@ -426,12 +443,17 @@ defmodule Examples.DSLMailboxSimple do
   """
   def standalone_mailbox_example do
     # Mailbox engines can run independently!
-    {:ok, mailbox_addr} = EngineSystem.spawn_engine(
-      SimpleFIFOMailbox,               # This is a mailbox engine
-      %{batch_size: 20, max_demand: 200}, # Configuration
-      %{},                             # Environment
-      :standalone_queue                # Name
-    )
+    {:ok, mailbox_addr} =
+      EngineSystem.spawn_engine(
+        # This is a mailbox engine
+        SimpleFIFOMailbox,
+        # Configuration
+        %{batch_size: 20, max_demand: 200},
+        # Environment
+        %{},
+        # Name
+        :standalone_queue
+      )
 
     mailbox_addr
   end
@@ -483,7 +505,8 @@ defmodule Examples.DSLMailboxSimple do
         mailbox_pid: priority_info.mailbox_pid,
         spec: priority_info.spec_key
       },
-      architecture: "Both engines use the same processing logic with different mailbox delivery policies!"
+      architecture:
+        "Both engines use the same processing logic with different mailbox delivery policies!"
     }
   end
 end
